e47efc117f70df8c2c93fa3f92181b8ca47fffa0
CrystalKhadka <220042@softwarica.edu.np>
2024-05-12 13:33:45 +0545

    changes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,9 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -12,37 +15,74 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  
+  var validate = () => {
+    var isValid = true;
+
+    // validate the first name
+
+    if (email.trim() === "") {
+      setEmailError("EMail is required");
+      isValid = false;
+    }
+    if (password.trim() === "") {
+      setPasswordError("Password is required");
+      isValid = false;
+    }
+
+    return isValid;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var isValidated = validate();
+    if (!isValidated) {
+      return;
+    }
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    console.log(data);
+  };
+
   return (
     <div className="container">
       <div className="form-container ">
         <h2 className="text-center mb-4">Log in</h2>
         <form className="m-auto">
           <div className="form-group">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email Address</label>
             <input
-              type="text"
-              className="form-control w-50"
+              type="email"
+              className="form-control"
               id="email"
               name="email"
               onChange={handleEmail}
               required
             />
+            {emailError && <p className="text-danger">{emailError}</p>}
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <input
-              type="text"
-              className="form-control w-50"
+              type="password"
+              className="form-control"
               id="password"
               name="password"
               onChange={handlePassword}
               required
             />
+            {passwordError && <p className="text-danger">{passwordError}</p>}
+
+            <button
+              type="submit"
+              className="btn btn-danger btn-block w-50 mt-5"
+              onClick={handleSubmit}
+            >
+              Login
+            </button>
           </div>
-          <button type="submit" className="btn btn-danger btn-block w-50 m-5">
-            Login
-          </button>
         </form>
       </div>
     </div>
